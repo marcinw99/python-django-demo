12aa801a788559fb55379e9781be694e2cdc4c5d
Marcin Wąsik <marcinmw99@gmail.com>
2024-05-22 11:18:28 +0200

    feat: polls models and migration

--- djangodemo/polls/models.py
+++ djangodemo/polls/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)  # this will be used both by db and django validation
+    publish_date = models.DateTimeField(
+        "date published")  # human-readable label, always first positional argument (optional)
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)  # SQL equivalent of ON DELETE CASCADE
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
