b5ed876d832e052bc6c76e7ee2e69a72f9c1b276
Marcin Wąsik <marcinmw99@gmail.com>
2024-05-22 12:08:35 +0200

    feat: readable models

--- djangodemo/polls/models.py
+++ djangodemo/polls/models.py
@@ -1,3 +1,7 @@
+import datetime
+
+from django.utils import timezone
+
 from django.db import models
 
 
@@ -6,8 +10,17 @@ class Question(models.Model):
     publish_date = models.DateTimeField(
         "date published")  # human-readable label, always first positional argument (optional)
 
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.publish_date >= timezone.now() - datetime.timedelta(days=1)
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)  # SQL equivalent of ON DELETE CASCADE
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.choice_text} {self.votes}"
